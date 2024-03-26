394d9af835caea6a36270bd75e36703180be6e95
jkim2828 <87334152+jkim2828@users.noreply.github.com>
2024-03-26 09:59:52 -0400

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/jkim2828/sciatpartner/partnersciat_main.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
