5cb1e596b4b7018361c4a4895db1c85c548d506c
jkim2828 <87334152+jkim2828@users.noreply.github.com>
2024-03-26 10:46:07 -0400

    Update test3.js

--- test3.js
+++ test3.js
@@ -9,17 +9,17 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/jkim2828/sciatpartner/partnerscia
 		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#31b404','font-size':'3em'}
-		  },	
+			media : [ //Stimuli
+				{word: 'Ptr1'},
+				{word: 'Ptr2'},
+				{word: 'Ptr3'},
+				{word: 'Ptr4'},
+				{word: 'Ptr5'},
+				{word: 'Ptr6'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
 
   		attribute1 : 
 			{
